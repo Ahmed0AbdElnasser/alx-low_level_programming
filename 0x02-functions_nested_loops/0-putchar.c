2d4fa02baf91149e1487c0b2488ab3e4844026b1
Ahmed0Abdelnasser <ahmdabdelnasser102@gmail.com>
2023-03-21 18:32:50 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-	/**
- * main - Entry point
- *
- * @parameter main: main
- * Return: Always 0 (Success)
- */
+/**
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 
 	int main(void)
 	{
